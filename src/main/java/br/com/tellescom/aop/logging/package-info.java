a6101190b0deec59befd823167408f3d2ddb9fa4
Victor Freire <victor.desenvolvedor@gmail.com>
2024-08-09 17:13:48 -0400

    Initial version of all4qmsMsMetaInd generated by generator-jhipster@8.1.0

---
+++ src/main/java/br/com/tellescom/aop/logging/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Logging aspect.
+ */
+package br.com.tellescom.aop.logging;
---
+++ src/main/java/br/com/tellescom/config/Constants.java
@@ -0,0 +1,11 @@
+package br.com.tellescom.config;
+
+/**
+ * Application constants.
+ */
+public final class Constants {
+
+    public static final String SYSTEM = "system";
+
+    private Constants() {}
+}
