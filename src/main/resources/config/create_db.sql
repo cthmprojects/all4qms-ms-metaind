9e9dddb9efe64577ef4bbdae73383110e30b4f2a
Victor Freire <victor.desenvolvedor@gmail.com>
2024-10-20 11:29:04 -0400

    Divisão do banco de dados

---
+++ src/main/resources/config/create_db.sql
@@ -0,0 +1,4 @@
+-- Criar banco e usuário para all4qmsMsAuditPlan
+CREATE USER all4qmsmsauditplan WITH PASSWORD 'all4qmsmsauditplan';
+CREATE DATABASE all4qmsmsauditplan OWNER all4qmsmsauditplan;
+GRANT ALL PRIVILEGES ON DATABASE all4qmsmsauditplan TO all4qmsmsauditplan;
